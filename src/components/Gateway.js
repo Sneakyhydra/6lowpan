321201f0d861fde8c19552c2de335d4e92614f0f
Sneakyhydra <dhruvrishishwar2@gmail.com>
2023-05-01 04:21:58 +0530

    Remove gauges

--- src/components/Gateway.js
+++ src/components/Gateway.js
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 
-import Gauge from './Gauge';
 import styles from '../styles/components/Gateway.module.css';
 
 import raspberry_pi_3 from '/public/img/raspberry_pi_3.png';
@@ -10,7 +9,7 @@ const Gateway = ({ gatewayData, conn }) => {
 		<div className={styles.gridContainer}>
 			{conn ? (
 				<>
-					<div className={styles.piSection}>
+					<div className={styles.piSection} style={{ gridColumn: '1 / span 2', placeItems: 'center' }}>
 						<div className={styles.imageContainer}>
 							<Image src={raspberry_pi_3} alt='' height={250} />
 							<div className={styles.imageTag}>Raspberry Pi 3A+</div>
@@ -56,10 +55,6 @@ const Gateway = ({ gatewayData, conn }) => {
 								</div>
 							</div>
 						</div>
-					</div>
-
-					<div>
-						<Gauge min={0} max={100} val={gatewayData['cpu-usage']} />
 					</div>
 				</>
 			) : (
